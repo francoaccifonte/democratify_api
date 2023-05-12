bcf3f15a7e48bb0bd6a81c7c64444714963f9bbc
FrancoAccifonte <franco.accifonte@gmail.com>
2023-05-12 14:41:50 -0300

    add serializer to application helper and test it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
 
     AccountSerializer.new(**options).serialize_to_json(account)
   end
+
+  def serialized_spotify_playlists(playlists, options: {})
+    Panko::ArraySerializer.new(
+      playlists,
+      **options.merge(each_serializer: SpotifyPlaylistSerializer)
+    ).to_json
+  end
 end
