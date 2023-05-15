88c2398732b798f2fe12dce850cba34020a11b40
FrancoAccifonte <franco.accifonte@gmail.com>
2023-05-14 23:34:16 -0300

    test all erb views

--- spec/views/accounts/signup.html.erb_spec.rb
+++ spec/views/accounts/signup.html.erb_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "welcome/index" do
+RSpec.describe "accounts/signup" do
   it 'renders the react component' do
+    assign(:failed_auth, false)
     allow(view).to receive(:react_component)
     render
     expect(view).to(
       have_received(:react_component)
-      .with('HomeHoc', prerender: false)
+      .with('SignupViewHoc',
+            prerender: false)
     )
   end
 end
--- spec/views/spotify_playlists/show.html.erb_spec.rb
+++ spec/views/spotify_playlists/show.html.erb_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "spotify_playlists/show" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:account) { create(:account) }
+  let!(:playlist) { create(:spotify_playlist) }
+
+  before do
+    assign(:account, account)
+    assign(:spotify_playlist, playlist)
+  end
+
+  it 'renders the react component' do
+    allow(view).to receive(:react_component)
+    render
+    expect(view).to(
+      have_received(:react_component)
+      .with('SpotifyPlaylistShowViewHoc',
+            props: {
+              account: AccountSerializer.new({ except: [:token] }).serialize_to_json(account),
+              playlist: SpotifyPlaylistSerializer.new.serialize_to_json(playlist)
+            },
+            prerender: false)
+    )
+  end
 end
