100cc587ad8e99631ff0123a79cbe7b4e66b39ac
FrancoAccifonte <franco.accifonte@gmail.com>
2023-05-17 22:07:55 -0300

    wip

--- app/controllers/ongoing_playlists_controller.rb
+++ app/controllers/ongoing_playlists_controller.rb
@@ -2,6 +2,8 @@ class OngoingPlaylistsController < ApplicationController
   # GET /ongoing_playlists
   def index
     @ongoing_playlist = @account.ongoing_playlist
+    return redirect_to spotify_playlists_url if @ongoing_playlist.nil?
+
     @votation = @ongoing_playlist.votations.where(in_progress: true).first
   end
 
