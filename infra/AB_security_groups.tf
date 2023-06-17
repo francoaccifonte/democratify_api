a22ae6d3be437cc881c6acac545f5d6a74c6432b
FrancoAccifonte <franco.accifonte@gmail.com>
2023-06-17 19:20:57 -0300

    fix internet access to ec2 y otras hierbas

--- infra/AB_security_groups.tf
+++ infra/AB_security_groups.tf
@@ -4,14 +4,23 @@ resource "aws_security_group" "rockolify_public" {
   description = "Allow internet traffic into public subnets"
   vpc_id      = aws_vpc.rockolify.id
 
+  ingress {
+    description = "attempt to allow ssh"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    self        = true
+    from_port   = 22
+    to_port     = 22
+  }
+
   tags = {
     Name = "rockolify-public"
   }
 }
 
 resource "aws_security_group" "rockolify_private" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
+  name        = "from_public_to_private"
+  description = "Allow traffic from public security group"
   vpc_id      = aws_vpc.rockolify.id
 
   tags = {
--- infra/AC_ec2.tf
+++ infra/AC_ec2.tf
@@ -1,9 +1,27 @@
 resource "aws_instance" "rockolify_app" {
-  ami           = "ami-0e820afa569e84cc1"
-  instance_type = "t3.micro"
-  subnet_id     = aws_subnet.rockolify_public.id
+  ami                    = "ami-0e820afa569e84cc1"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.rockolify_public.id
+  vpc_security_group_ids = [aws_security_group.rockolify_public.id]
 
   tags = {
     Name = "Rockolify-app"
   }
+}
+
+resource "aws_eip" "rockolify_ec2_ip" {
+  instance = aws_instance.rockolify_app.id
+  domain   = "vpc"
+}
+
+resource "aws_key_pair" "terraform_ec2_key" {
+  key_name   = "terraform_ec2_key"
+  public_key = file("terraform_ec2_key.pub")
+}
+
+################# OUTPUTS #########################################################
+
+output "ec2_elastic_ip" {
+  value       = aws_eip.rockolify_ec2_ip.public_ip
+  description = "The public IP taken from the elastic ip resource"
 }
