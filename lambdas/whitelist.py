b528c9339d0065ff624f6ed44ade24bcda4794d0
FrancoAccifonte <franco.accifonte@gmail.com>
2023-07-24 18:59:18 -0300

    [codebuildLambda]

--- lambdas/whitelist.py
+++ lambdas/whitelist.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from tempfile import mkdtemp
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 
 def handler(event=None, context=None):
-    print(os.environ['SPOTIFY_ACCOUNT_EMAIL'])
     options = webdriver.ChromeOptions()
     options.binary_location = '/opt/chrome/chrome'
     options.add_argument('--headless')
@@ -19,10 +20,33 @@ def handler(event=None, context=None):
     options.add_argument(f"--data-path={mkdtemp()}")
     options.add_argument(f"--disk-cache-dir={mkdtemp()}")
     options.add_argument("--remote-debugging-port=9222")
-    chrome = webdriver.Chrome("/opt/chromedriver",
+    driver = webdriver.Chrome("/opt/chromedriver",
                               options=options)
-    chrome.get("https://example.com/")
-    
-    return chrome.find_element(by=By.XPATH, value="//html").text
 
-handler('123', '123')
+    account_id = event["account_id"]
+    email = event["email"]
+    account_name = event["account_name"]
+
+    wait = WebDriverWait(driver, 15)
+
+    driver.get('https://developer.spotify.com/')
+    wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/header/div[2]/button'))).click()
+    wait.until(EC.presence_of_element_located((By.ID, 'login-username'))).send_keys(os.environ['SPOTIFY_ACCOUNT_EMAIL'])
+    wait.until(EC.presence_of_element_located((By.ID, 'login-password'))).send_keys(os.environ['SPOTIFY_ACCOUNT_PASSWORD'])
+    driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div/div/div[1]/div[4]/button/span[1]').click()
+
+    wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/header/div[2]/div/div/button/span')))
+
+    driver.get('https://developer.spotify.com/dashboard/9d48abfbbf194adc9051e1b82b0ecdb0/users')
+    name_input = wait.until(EC.presence_of_element_located((By.ID, 'name')))
+    name_input.send_keys(f"{account_name}{account_id}" if account_name else f"namelessUser{account_id}")
+    email_input = wait.until(EC.presence_of_element_located((By.ID, 'email')))
+    email_input.send_keys(email)
+    driver.find_element(By.XPATH, '/html/body/div[1]/div/div/main/div/div/div[4]/div/div/div[2]/form/div/div[3]/button/span[1]').click()
+
+    # Add a sleep if necessary
+    # time.sleep(3)
+
+    driver.quit()
+    
+    return "ok"
