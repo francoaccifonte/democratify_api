44ad0d97013f5529efe81ddfa13ddf9654b69498
FrancoAccifonte <franco.accifonte@gmail.com>
2023-08-21 11:10:02 -0300

    fix tests

--- spec/requests/spotify_playlists_spec.rb
+++ spec/requests/spotify_playlists_spec.rb
@@ -7,50 +7,51 @@ RSpec.describe '/spotify_playlists' do
     context 'when user is logged in' do
       before { WithAccountCookies.set_account_cookie(cookies, account) }
 
-    context 'when account has a spotify user' do
-      let!(:spotify_user) { create(:spotify_user, account:) }
+      context 'when account has a spotify user' do
+        let!(:spotify_user) { create(:spotify_user, account:) }
 
-      context 'with existing spotify_playlists' do
-        let!(:spotify_playlists) { create(:spotify_playlist, account:) }
+        context 'with existing spotify_playlists' do
+          let!(:spotify_playlists) { create(:spotify_playlist, account:) }
 
-        it 'returns http success' do
-          get spotify_playlists_url
-          expect(response).to have_http_status(:success)
+          it 'returns http success' do
+            get spotify_playlists_url
+            expect(response).to have_http_status(:success)
+          end
+
+          it 'assigns the correct component props' do
+            get spotify_playlists_url
+            expect(controller.instance_variable_get(:@component_props)).to include(
+              {
+                account: anything,
+                playlists: anything
+              }
+            )
+          end
         end
 
-        it 'assigns the correct component props' do
-          get spotify_playlists_url
-          expect(controller.instance_variable_get(:@component_props)).to include(
-            {
-              account: anything,
-              playlists: anything
-            }
-          )
+        context 'without existing spotify_playlists' do
+          it 'returns http success' do
+            get spotify_playlists_url
+            expect(response).to have_http_status(:success)
+          end
+        end
+
+        context 'with invalid cookies' do
+          it 'redirects to login and clears cookies' do
+            account.update!(token: 'fakeToken')
+            get spotify_playlists_url
+            expect(response).to redirect_to(root_url)
+            expect(response.cookies[:account_id]).to be_nil
+            expect(response.cookies[:token]).to be_nil
+          end
         end
       end
 
-      context 'without existing spotify_playlists' do
-        it 'returns http success' do
+      context 'when account does not have a spotify user' do
+        it 'redirects to account settings' do
           get spotify_playlists_url
-          expect(response).to have_http_status(:success)
+          expect(response).to redirect_to(account_settings_url)
         end
-      end
-
-      context 'with invalid cookies' do
-        it 'redirects to login and clears cookies' do
-          account.update!(token: 'fakeToken')
-          get spotify_playlists_url
-          expect(response).to redirect_to(root_url)
-          expect(response.cookies[:account_id]).to be_nil
-          expect(response.cookies[:token]).to be_nil
-        end
-      end
-    end
-
-    context 'when account does not have a spotify user' do
-      it 'redirects to account settings' do
-        get spotify_playlists_url
-        expect(response).to redirect_to(account_settings_url)
       end
     end
   end
